7dfd06ea59e7a58c7955dfab36686ab9ba963b4b
DiegoAlan12 <56890121+DiegoAlan12@users.noreply.github.com>
2022-12-02 14:17:36 -0600

    MayorAvance

--- Codigo/prueba.py
+++ Codigo/prueba.py
@@ -1,23 +1,26 @@
 import random
-import time
-
 def pobIni(num):
     listaInd = []
     listaIndEva = []
+    listaAll = []
     for i in range(num):
         ind = [random.randint(0, 1) for _ in range(12)]
         print(f'{ind}')
-        listInd.append(ind)
+        listaInd.append(ind)
         listaIndEva.append(evaluation(ind))
+        listaAll.append(ind)
+        listaAll.append(evaluation(ind))
     print(listaInd)
     print(listaIndEva)
-    return listaInd, listaIndEva
-        
+    print(listaAll)
+    #print("{:.2f}".format(listaIndEva))
+    return listaInd, listaIndEva, listaAll
+
 def evaluation(ind):
     #Color del pozole
     char1 =[]
     char1.append(ind[0])
-    print(char1)
+#   print(char1)
 
     rojo = [0]
     blanco = [1]
@@ -25,12 +28,11 @@ def evaluation(ind):
         peso1 = 6.4
     elif char1 == blanco:
         peso1 = 6.1
-    print(peso1)
 
     #Lechuga Si/No
     char2 =[]
     char2.append(ind[1])
-    print(char2)
+#    print(char2)
 
     lechugaSi = [0]
     lechugaNo = [1]
@@ -38,12 +40,11 @@ def evaluation(ind):
         peso2 = 7.3
     elif char2 == lechugaNo:
         peso2 = 5.2
-    print(peso2)
 
     #Oregano Si/No
     char3 =[]
     char3.append(ind[2])
-    print(char3)
+#    print(char3)
 
     oreganoSi = [0]
     oreganoNo = [1]
@@ -51,12 +52,11 @@ def evaluation(ind):
         peso3 = 8.0
     elif char3 == oreganoNo:
         peso3 = 4.5
-    print(peso3)
     
     #Acuagate
     char4 = ind[3:5]
 
-    print(char4)
+#    print(char4)
     
     aHass = [1,1]
     aLHass = [1,0]
@@ -70,11 +70,10 @@ def evaluation(ind):
         peso4 = 1.1
     elif char4 == aFuerte:
         peso4 = 3.1
-    print(peso4)
 
     #Limon
     char5 = ind[5:7]
-    print(char5)
+#    print(char5)
     
     lSSemilla = [1,1]
     lVerde = [1,0]
@@ -88,12 +87,11 @@ def evaluation(ind):
         peso5 = 1.1
     elif char5 == lReal:
         peso5 = 5.2
-    print(peso5)
 
     #Rabano Si/No
     char6 =[]
     char6.append(ind[7])
-    print(char6)
+#    print(char6)
 
     rabanoSi = [0]
     rabanoNo = [1]
@@ -101,12 +99,11 @@ def evaluation(ind):
         peso6 = 7.5
     elif char6 == lechugaNo:
         peso6 = 5.0
-    print(peso6)
     
     #Carne Puerco/Pollo
     char7 =[]
     char7.append(ind[8])
-    print(char7)
+#    print(char7)
 
     puerco = [0]
     pollo = [1]
@@ -114,12 +111,10 @@ def evaluation(ind):
         peso7 = 9.1
     elif char7 == pollo:
         peso7 = 3.4
-    print(peso7)
-
     #Salsa
     char8 = ind[9:12]
     
-    print(char8)
+#    print(char8)
 
     sVerde = [0,0,0]
     sRoja = [0,0,1]
@@ -145,7 +140,6 @@ def evaluation(ind):
         peso8 = 1.0
     elif char8 == sMorita:
         peso8 = 2.0
-    print(peso8)
 
     FaF = peso1 + peso2 + peso3 + peso4 + peso5 + peso6 + peso7 + peso8
     """"
@@ -190,14 +184,20 @@ def ruleta(lista):
 
     return listax  
 
-
 if __name__ == "__main__":
     NG = int(input("Número de generaciónes: \n"))
     NI = int(input("Número de individuos: \n"))
-    Ttor = int(input("Tamanio del torneo: \n"))
-    
-    for i in range(NG):
-        lista = pobIni(NI)
-        fin = sorted(lista)
-        print("Individuo: ", i)
 
+    for i in range(NG):
+        listaInd, listaIndEva, listaAll = pobIni(NI)
+        listaX = ruleta(listaIndEva)
+        print("Individuos", listaInd)
+        print("Evaluados", listaIndEva)
+        #print("{0:.2f}".format(listaX))
+        print("Ruleta", listaX)
+        print("Individuos y su evaluación: ", listaAll)
+
+        for indice, (item1,item2) in enumerate(zip(listaX, listaIndEva)):
+            
+            if item1==item2:
+                print(f"{item1} se encuentra en ambas listas en el índice {indice}")
